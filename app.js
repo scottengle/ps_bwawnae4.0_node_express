00a08560b966ad745b23ea11ee46a52958b329e6
Scott Engle <scottengle22@gmail.com>
2017-01-11 19:57:09 -0700

    Module 3: Setting Up Gulp

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
@@ -15,6 +15,6 @@ app.get('/', function(req, res) {
   res.send('hello world!');
 });
 
-app.listen(5000, function(err) {
+app.listen(port, function(err) {
   console.log('running server on port', port);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     jscs = require('gulp-jscs'),
+    nodemon = require('gulp-nodemon'),
     jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function() {
@@ -14,12 +15,38 @@ gulp.task('style', function() {
 
 gulp.task('inject', function() {
   var wiredep = require('wiredep').stream,
+      inject = require('gulp-inject'),
       options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-      }
+      },
+      injectSrc = gulp.src(['./public/css/*.css',
+                            './public/js/*.js'], {read: false}),
+      injectOptions = {
+        ignorePath: '/public'
+      };
+
   return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 3000
+    },
+    watch: jsFiles
+  };
+
+  return nodemon(options)
+      .on('restart',
+          function(ev) {
+            console.log('Restarting...');
+          }
+      );
 });
